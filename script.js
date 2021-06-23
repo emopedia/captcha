dbeb3095c208798f08b197ba76e34ecfcbaf733d
emopedia <76584247+emopedia@users.noreply.github.com>
2021-06-23 20:13:40 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var captcha = document.getElementById("fake-captcha");
 
 var passOrFail = function() {
   var pass = Math.round(Math.random());
-  return pass ? "fail";
+  return pass ? "pass" : "fail" : "fail" : "fail";
 }
 
 captcha.onclick = function() {
